b19df0c0224a2f21c4f8d8678c42d939716cb528
BOCKELADNT Amelie <ameliebock@hotmail.fr>
2021-03-06 14:45:57 +0100

    adding header component and css

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// style
+import "../style/header.css"
+
+const header = () =>
+{
+    return (
+        <header>
+            <h1>test</h1>
+        </header>
+    );
+}
+
+export default header;
