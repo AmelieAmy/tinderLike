c2dc7c7cfcd4938d4e23b7fa75d1715844a040c8
BOCKELADNT Amelie <ameliebock@hotmail.fr>
2021-03-06 15:08:51 +0100

    rectification path for logo in header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,24 @@ import React from 'react';
 // style
 import "../style/header.css"
 
+// assets
+import PersonIcon from '@material-ui/icons/Person';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import IconButton from '@material-ui/core/IconButton';
+import Logo from '../assets/tinderLikeLogo.svg'
+
+
 const header = () =>
 {
     return (
         <header>
-            <h1>test</h1>
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icons" />
+            </IconButton>
+            <img src={Logo} alt="blabla" />
+            <IconButton>
+                <QuestionAnswerIcon fontSize="large" className="header__icons" />
+            </IconButton>
         </header>
     );
 }
