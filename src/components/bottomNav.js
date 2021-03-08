cb488f70bb6331fec0255cc5a70a07ce361f751a
BOCKELADNT Amelie <ameliebock@hotmail.fr>
2021-03-08 12:16:37 +0100

    Slightly changing all css of the app and the bottomNav

--- src/components/bottomNav.js
+++ src/components/bottomNav.js
@@ -1,16 +1,36 @@
 import React  from 'react';
 
 // assets
+import ReplayIcon from "@material-ui/icons/Replay";
+import CloseIcon from "@material-ui/icons/Close";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import IconButton from '@material-ui/core/IconButton';
 
 // styles
-import "../style/tinderCards.css"
+import "../style/bottomNav.css"
 
 
 const bottomNav = () =>
 {
     return(
-        <div>
-            <h2>Buttons go here</h2>
+        <div className="BottomNav">
+            <IconButton className="bottomNav__repeat" >
+                <ReplayIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="bottomNav__left" >
+                <CloseIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="bottomNav__star" >
+                <StarRateIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="bottomNav__right" >
+                <FavoriteIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="bottomNav__lightning" >
+                <FlashOnIcon fontSize="large" />
+            </IconButton>
         </div>
     )
 }
