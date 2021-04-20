4dd4d74532a21d0c0c2f4e59a1c849b41e9ab10b
BOCKELADNT Amelie <ameliebock@hotmail.fr>
2021-04-20 15:40:21 +0200

    Updating account page

---
+++ src/components/ACCOUNT/Account.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+// components
+import Header from '../header'
+import ProfileInfos from './accountInfos'
+import ProfileNavBar from './accountNavBar'
+
+// assets
+
+// styles
+
+
+const Account = () =>
+{
+    return(
+        <>
+            <Header />
+            <ProfileInfos />
+            {/* <ProfileNavBar /> */}
+        </>
+    )
+}
+
+export default Account;
